916014420ab044e7976c99117c85a5190ce74010
isaac <isaac@quemais.com.br>
2021-01-10 15:03:18 -0300

    learning about raw SQL Queries on Laravel

--- base-app/routes/web.php
+++ base-app/routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -15,4 +14,36 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+/**
+ * Database simple examples routes
+ */
+use Illuminate\Support\Facades\DB;
+ Route::get('/insert',function(){
+    DB::insert("INSERT INTO posts(title, content) VALUES(?,?)",['esse é um dado inserido com laravel',"esse é o conteúdo."]);
+ });
+
+ Route::get('/readAll',function(){
+     $array =[];
+     $array= DB::select("SELECT * FROM posts");
+    print_r($array);
+ });
+
+ 
+ Route::get('/readOne',function(){
+    $array =[];
+    $array= DB::select("SELECT * FROM posts WHERE id = ?",[1]);
+   print_r($array[0]->title);
+});
+
+Route::get('/update',function(){
+    $updated = DB::update('UPDATE posts SET title = "titulo atualizado" WHERE id = ?',[1]);
+    print_r($updated);
+});
+
+Route::get('/delete',function(){
+    $deleted = DB::delete('DELETE FROM posts WHERE id = ?',[1]);
+    print_r($deleted);
 });
