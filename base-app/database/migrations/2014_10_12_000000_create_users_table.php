bc9dcf34cd0d0bac1b4e883cdf0863fa9c12b95c
isaac <isaac@quemais.com.br>
2021-01-10 15:06:04 -0300

    learning about laravel migrations, pretty cool feature!

--- base-app/database/migrations/2014_10_12_000000_create_users_table.php
+++ base-app/database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,18 @@
 <?php
 
+
+
+/**
+ * Abaixo um exemplo de migration em laravel.
+ * 
+ * Considere as migrações como um "git" para databases,
+ * cada migration conta com um método "up" e "down", que são exatamente isso,
+ * o que a migração deve fazer se ela for executada normalmente e o down
+ * para o caso de ser um rollback. é basicamente o ato de vc criar algo na
+ * estrutura do seu banco, mas também ter um meio de jogar fora essa estrutura
+ * caso por alguma razão você precise que seu sistema volte a um estado anterior.
+ */
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -10,6 +23,15 @@ class CreateUsersTable extends Migration
      * Run the migrations.
      *
      * @return void
+     * 
+     * Abaixo, o método up, que diz o que vai ter de novo na estrutura do DB após essa migration ser executada.
+     * 
+     * Essa migration em particular usa a função create, ou seja, uma tabela nova será criada.
+     * 
+     * esse método tem 2 parâmetros, o nome da table (users) e uma função de callback
+     * cujo parâmetro é uma varivável do tipo Blueprint, que é a classe que define os valores
+     * de cada coluna dessa nova table. Cada método de uma instancia Blueprint equivale a uma
+     * coluna, o nome do metodo sendo o tipo de dado, e os parametros os nomes das colunas.
      */
     public function up()
     {
@@ -28,6 +50,12 @@ class CreateUsersTable extends Migration
      * Reverse the migrations.
      *
      * @return void
+     * 
+     * 
+     * Abaixo, um método down, que ao contrário do up (duhhh) serve pra quando vc quer desfazer
+     * as alterações que vc faz no up. como nesse up em específico criamos a table users,
+     * basta chamarmos o método de dropar uma table, passando o nome users como parâmetro para identificar
+     * a tabela a ser excluída.
      */
     public function down()
     {
